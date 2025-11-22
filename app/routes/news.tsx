65de433f785256cc14b28dc68299f1e987c10da9
MatchaTi <tehmatcha28@gmail.com>
2025-11-22 09:52:52 +0800

    feat: add SEO metadata to News page for improved visibility

--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -3,6 +3,14 @@ import News from '@/components/ui/news';
 export default function NewsPage() {
 	return (
 		<div className='space-y-32 py-32'>
+			<title>
+				Berita Terbaru Asisten Laboratorium Informatika | ASCII
+			</title>
+			<meta
+				name='description'
+				content='Dapatkan informasi terbaru seputar praktikum, termasuk pengumuman, berita, dan update penting lainnya.'
+			/>
+
 			<News />
 		</div>
 	);
